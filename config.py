717dd99f9426b7251ba84f0f6604b5f629d5e3ff
Markpo2 <127012533+Markpo2@users.noreply.github.com>
2023-03-10 00:53:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,26 @@
-# Copyright (c) 2022 Itz-fork
-
-import os
-
 class Config(object):
-    # Mandotory
-    API_ID = int(os.environ.get("7143337"))
-    API_HASH = os.environ.get("1afa55a5f3bf7058c843d1b290f79c49")
-    BOT_TOKEN = os.environ.get("6043283784:AAHLV11M9g3gaDj5dE-Sr5fKhSCd8CT-lOc")
-    LOGS_CHANNEL = int(os.environ.get("-1001465954010"))
-    BOT_OWNER = int(os.environ.get("1284818583"))
-    MONGODB_URL = os.environ.get("MONGODB_URL")
-    GOFILE_TOKEN = os.environ.get("bbZVUX1q5HxuXR9AbkHIQrvimRmPH50N")
-    # Optional
-    MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
-    # Constents
-    DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/NexaBots"
-    TG_MAX_SIZE = 2040108421
-    CHUNK_SIZE = 1024 * 6
+
+    # get a token from @BotFather
+    BOT_TOKEN = "6043283784:AAHLV11M9g3gaDj5dE-Sr5fKhSCd8CT-lOc"
+
+    # Get these values from my.telegram.org
+    API_ID = 7143337
+    API_HASH = "1afa55a5f3bf7058c843d1b290f79c49"
+
+    # No need to change
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    ADL_BOT_RQ = {}
+    CHUNK_SIZE = 128
+    TG_MAX_FILE_SIZE = 4194304000
+    HTTP_PROXY = ""
+    PROCESS_MAX_TIMEOUT = 0
+
+    # TG Ids
+    LOG_CHANNEL = -1001465954010
+    OWNER_ID = 1284818583
+
+    # bot username without @
+    BOT_USERNAME = "uploaderMar_bot"
+
+    # auth users
+    AUTH_USERS = [1284818583]
